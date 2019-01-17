8259325e8be414e57879eaa383cefd4093863231
Ken Kousen <ken.kousen@kousenit.com>
2019-01-17 10:10:46 -0500

    Add reactive client to JokeService

--- restclient/src/main/groovy/com/kousenit/restclient/services/JokeService.groovy
+++ restclient/src/main/groovy/com/kousenit/restclient/services/JokeService.groovy
@@ -5,11 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
 
 @Service
 class JokeService {
     public static final String BASE = 'http://api.icndb.com/jokes/random?limitTo=[nerdy]'
     RestTemplate restTemplate
+    WebClient client = WebClient.create('http://api.icndb.com')
 
     @Autowired
     JokeService(RestTemplateBuilder builder) {
@@ -21,4 +24,14 @@ class JokeService {
         JokeResponse jokeResponse = restTemplate.getForObject(url, JokeResponse)
         return jokeResponse.value.joke
     }
+
+    Mono<String> getJokeAsync(String first, String last) {
+        String path = '/jokes/random?limitTo=[nerdy]&firstName={first}&lastName={last}'
+        return client.get()
+                .uri(path, first, last)
+                .retrieve()
+                .bodyToMono(JokeResponse)
+                .map { jokeResponse -> jokeResponse.value.joke }
+    }
+
 }
--- restclient/src/test/groovy/com/kousenit/restclient/services/JokeServiceTest.groovy
+++ restclient/src/test/groovy/com/kousenit/restclient/services/JokeServiceTest.groovy
@@ -7,6 +7,9 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
+import reactor.test.StepVerifier
+
+import java.time.Duration
 
 @RunWith(SpringRunner)
 @SpringBootTest
@@ -21,6 +24,24 @@ class JokeServiceTest {
         String joke = service.getJoke("Craig", "Walls")
         logger.info(joke)
         assert joke.contains("Craig") || joke.contains("Walls")
+    }
+
+    @Test
+    void getJokeAsync() {
+        String joke = service.getJokeAsync('Craig', 'Walls')
+                .block(Duration.ofSeconds(2))
+        logger.info(joke);
+        assert joke.contains("Craig") || joke.contains("Walls")
+    }
+
+    @Test
+    void useStepVerifier() {
+        StepVerifier.create(service.getJokeAsync("Craig", "Walls"))
+                .assertNext { joke ->
+            logger.info(joke)
+            assert joke.contains('Craig') || joke.contains('Walls')
+        }
+        .verifyComplete()
     }
 
 }
